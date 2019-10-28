daed38291fcc8304e56389893b83dfa5f95c830b
Thiago Oliveira <thiago.oliveira@tap4mobile.com.br>
2019-10-28 15:25:55 -0400

    add validation email and password

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,8 +3,15 @@ const User = require('../models/User');
 const router = express.Router();
 
 router.post('/register', async(req, res) => {
+    const { email } = req.body;
+
     try {
+        if (await User.findOne({ email }))
+            return res.status(400).send({ error: 'User alredy resgistred' });
+
         const user = await User.create(req.body);
+
+        user.password = undefined;
 
         return res.send({ user });
     } catch (err) {
