90b144106daf6271bce91f36047cf4edcdbd5471
Thiago Oliveira <thiago.oliveira@tap4mobile.com.br>
2019-10-28 15:56:11 -0400

    add authenticate route

--- controllers/authController.js
+++ controllers/authController.js
@@ -19,4 +19,18 @@ router.post('/register', async(req, res) => {
     }
 });
 
+router.post('/authenticate', async(req, res) => {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email }).select('+password');
+
+    if (!user)
+        return res.status(400).send({ error: 'User not found' });
+
+    if (!await bcrypt.compare(password, user.password))
+        return res.status(400).send({ error: 'Ivalid password' });
+
+        res.send( { user });
+});
+
 module.exports = app => app.use('/auth', router);
