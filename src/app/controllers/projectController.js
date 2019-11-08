00c93663e3ef4499de18a5dd4280a9f905ca05f2
Thiago Oliveira <thiago.oliveira@tap4mobile.com.br>
2019-11-08 10:57:33 -0400

    add save tasks in project

--- src/app/controllers/projectController.js
+++ src/app/controllers/projectController.js
@@ -20,7 +20,7 @@ router.get('/', async (req, res) => {
 
 router.get('/:projectId', async (req, res) => {
     try {
-        const project = await Project.findById(req.params.projectId).populate('user');
+        const project = await Project.findById(req.params.projectId).populate(['user', 'tasks']);
 
         return res.send({ project });
     } catch (err) {
@@ -30,7 +30,19 @@ router.get('/:projectId', async (req, res) => {
 
 router.post('/', async (req, res) => {
     try {
-        const project = await Project.create({ ...req.body, user: req.userId });
+        const { title, description, tasks } = req.body;
+
+        const project = await Project.create({ title, description, user: req.userId });
+
+        await Promise.all(tasks.map(async task => {
+            const projectTask = new Task({ ...task, project: project._id })
+
+            await projectTask.save();
+
+            project.tasks.push(projectTask);
+        }));
+
+        await project.save();
 
         return res.send({ project });
     } catch (err) {
