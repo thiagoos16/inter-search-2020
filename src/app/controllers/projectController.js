5a989e9bd615d15199d9575cb308c2cb60fa3bc8
Thiago Oliveira <thiago.oliveira@tap4mobile.com.br>
2019-11-08 09:41:10 -0400

    add create and list all projects

--- src/app/controllers/projectController.js
+++ src/app/controllers/projectController.js
@@ -1,12 +1,43 @@
 const express = require('express');
 const authMiddleware = require('../middlewares/auth');
 
+const Project = require('../models/project');
+const Task = require('../models/task');
+
 const router = express.Router();
 
 router.use(authMiddleware);
 
-router.get('/', (req, res) => {
-    res.send({ ok: true, user: req.userId });
+router.get('/', async (req, res) => {
+    try {
+        const projects = await Project.find().populate('user');
+
+        return res.send({ projects });
+    } catch (err) {
+        return res.status(400).send({ error: 'Error loading projetcs' });
+    }
+});
+
+router.get('/:projectId', async (req, res) => {
+    res.send({ user: req.userId });
+});
+
+router.post('/', async (req, res) => {
+    try {
+        const project = await Project.create({ ...req.body, user: req.userId });
+
+        return res.send({ project });
+    } catch (err) {
+        return res.status(400).send({ error: 'Error creating new projetc' });
+    }
+});
+
+router.put('/:projectId', async (req, res) => {
+    res.send({ user: req.userId });
+});
+
+router.delete('/:projectId', async (req, res) => {
+    res.send({ user: req.userId });
 });
 
 module.exports = app => app.use('/projects', router);
